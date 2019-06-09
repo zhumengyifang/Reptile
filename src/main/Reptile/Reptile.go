d7d9db89aad09f3b063f24ef84cb9842b7620045
Jarvis <wangjiayuzh@163.com>
2019-06-09 23:19:15 +0800

    第一版channel

--- src/main/Reptile/Reptile.go
+++ src/main/Reptile/Reptile.go
@@ -7,9 +7,10 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"sync/atomic"
+	"time"
 )
 
-//目标网址
 const blogUrl = "https://blog.csdn.net/weixin_40165163/article/list/"
 const existUrl = "weixin_40165163"
 
@@ -18,46 +19,40 @@ func GetBlogVisitCount(url string) {
 		url = blogUrl
 	}
 
-	//统计总的访问量
-	count := 0
-	//判断是否为最后一页
-	isLastUrl := false
-	for i := 1; !isLastUrl; i++ {
-		n := 0
-		//循环拼下一页的url
+	ch := make(chan int, 10)
+	request := make(chan int32)
+
+	for i := 1; i <= 5; i++ {
 		url := blogUrl + strconv.Itoa(i) + "?"
 		fmt.Println("开始爬取：", url)
-		//按页去统计访问量count
-		isLastUrl, n = GetPageBlogCount(url)
-		//汇总
-		count += n
+		go getPageBlogCount(url, ch)
 	}
-	fmt.Println("总访问量：", count)
+
+	go getResult(ch, request)
+	fmt.Println("统计结果： ", <-request)
 }
 
-func GetPageBlogCount(url string) (bool, int) {
-	//获取需要爬取url的document对象
-	doc := GetNewDoc(url)
+func getPageBlogCount(url string, ch chan int) {
+	doc := getNewDoc(url)
 
-	//获取当前页面所有博客的连接和title
-	m, isLastUrl := GetBlog(doc)
-	//如果当前页面爬不到url则为最后一页结束循环
+	m, isLastUrl := getBlog(doc)
 	if isLastUrl {
-		return true, 0
+
 	}
 
-	count := 0
 	for k, v := range m {
-		//获取详细博客内容中的访问量
-		s, n := GetVisitCount(GetNewDoc(k))
-		fmt.Println("title:", v, "  ", s, "int:", n)
-		count += n
+		go getChildCount(k, v, ch)
 	}
-	return false, count
 }
 
-//获取url地址的document对象
-func GetNewDoc(url string) *goquery.Document {
+func getChildCount(url string, title string, ch chan int) {
+	doc1 := getNewDoc(url)
+	s, n := getVisitCount(doc1)
+	fmt.Println("title:", title, "  ", s, "int:", n)
+	ch <- n
+}
+
+func getNewDoc(url string) *goquery.Document {
 	if url == "" {
 		panic("Url Is Nil")
 	}
@@ -78,8 +73,20 @@ func GetNewDoc(url string) *goquery.Document {
 	return doc
 }
 
-//获取doc对象中的url和title
-func GetBlog(doc *goquery.Document) (map[string]string, bool) {
+func getResult(ch chan int, request chan int32) {
+	time.Sleep(time.Second)
+	var num int32 = 0
+	for {
+		select {
+		case t := <-ch:
+			atomic.AddInt32(&num, int32(t))
+		default:
+			request <- atomic.LoadInt32(&num)
+		}
+	}
+}
+
+func getBlog(doc *goquery.Document) (map[string]string, bool) {
 	isLastUrl := true
 	if doc == nil {
 		panic("Doc Is Nil")
@@ -97,13 +104,12 @@ func GetBlog(doc *goquery.Document) (map[string]string, bool) {
 	return m, isLastUrl
 }
 
-func GetVisitCount(doc *goquery.Document) (string, int) {
+func getVisitCount(doc *goquery.Document) (string, int) {
 	visitCount := doc.Find(".read-count").Text()
-	return visitCount, GetCount(visitCount, "：")
+	return visitCount, getCount(visitCount, "：")
 }
 
-//解析 访问量：123 中的 123
-func GetCount(visitCount string, subStr string) int {
+func getCount(visitCount string, subStr string) int {
 	s := strings.Index(visitCount, subStr)
 	n, err := strconv.Atoi(visitCount[s+len(subStr):])
 	if err != nil {
