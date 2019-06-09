9e94646642ea1810bc1ac40daee951d09c1b7d0a
Jarvis <wangjiayuzh@163.com>
2019-06-09 22:33:32 +0800

    添加注释并统计count

--- src/main/Reptile/Reptile.go
+++ src/main/Reptile/Reptile.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 )
 
+//目标网址
 const blogUrl = "https://blog.csdn.net/weixin_40165163/article/list/"
 const existUrl = "weixin_40165163"
 
@@ -17,28 +18,45 @@ func GetBlogVisitCount(url string) {
 		url = blogUrl
 	}
 
+	//统计总的访问量
+	count := 0
+	//判断是否为最后一页
 	isLastUrl := false
 	for i := 1; !isLastUrl; i++ {
+		n := 0
+		//循环拼下一页的url
 		url := blogUrl + strconv.Itoa(i) + "?"
 		fmt.Println("开始爬取：", url)
-		isLastUrl = GetPageBlogCount(url)
+		//按页去统计访问量count
+		isLastUrl, n = GetPageBlogCount(url)
+		//汇总
+		count += n
 	}
+	fmt.Println("总访问量：", count)
 }
 
-func GetPageBlogCount(url string) bool {
+func GetPageBlogCount(url string) (bool, int) {
+	//获取需要爬取url的document对象
 	doc := GetNewDoc(url)
+
+	//获取当前页面所有博客的连接和title
 	m, isLastUrl := GetBlog(doc)
+	//如果当前页面爬不到url则为最后一页结束循环
 	if isLastUrl {
-		return true
+		return true, 0
 	}
 
+	count := 0
 	for k, v := range m {
+		//获取详细博客内容中的访问量
 		s, n := GetVisitCount(GetNewDoc(k))
 		fmt.Println("title:", v, "  ", s, "int:", n)
+		count += n
 	}
-	return false
+	return false, count
 }
 
+//获取url地址的document对象
 func GetNewDoc(url string) *goquery.Document {
 	if url == "" {
 		panic("Url Is Nil")
@@ -60,6 +78,7 @@ func GetNewDoc(url string) *goquery.Document {
 	return doc
 }
 
+//获取doc对象中的url和title
 func GetBlog(doc *goquery.Document) (map[string]string, bool) {
 	isLastUrl := true
 	if doc == nil {
@@ -83,6 +102,7 @@ func GetVisitCount(doc *goquery.Document) (string, int) {
 	return visitCount, GetCount(visitCount, "：")
 }
 
+//解析 访问量：123 中的 123
 func GetCount(visitCount string, subStr string) int {
 	s := strings.Index(visitCount, subStr)
 	n, err := strconv.Atoi(visitCount[s+len(subStr):])
