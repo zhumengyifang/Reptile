f583eb3ddb71b506fa5b3d3e58beb72f01ee1894
jiayu <wangjiayuzh@163.com>
2019-06-10 11:43:09 +0800

    增加计时器百分比功能

--- src/main/main.go
+++ src/main/main.go
@@ -1,13 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"goReptile/src/main/Reptile"
 	"time"
 )
 
 func main() {
 	for {
+		fmt.Println("Start")
+
+		ch := make(chan int)
 		Reptile.GetBlogVisitCount("")
-		time.Sleep(time.Minute * 30)
+		go loading(ch)
+		time.Sleep(time.Minute * 15)
+		ch <- 1
 	}
+}
+
+func loading(ch chan int, ) {
+	k := 0
+	n := 0
+	fmt.Print("Loading")
+	go func() {
+		for {
+			select {
+			case <-ch:
+				fmt.Println()
+				fmt.Println("Loading Over")
+				return
+			default:
+				time.Sleep(time.Second)
+				if k == 10 {
+					k = 0
+					n += 1
+					fmt.Print(n)
+				} else {
+					fmt.Print(".")
+					k += 1
+				}
+			}
+		}
+	}()
 }
